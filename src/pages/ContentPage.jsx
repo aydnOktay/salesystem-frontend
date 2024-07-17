71af83c4765455e8a71d6c14bce35dbcfa28ce90
aydnOktay <xaydinoktay@gmail.com>
2024-07-17 14:57:40 +0300

    There are some errors on the Contacts page, I will investigate these errors , 90% finished

--- src/pages/ContentPage.jsx
+++ src/pages/ContentPage.jsx
@@ -1,5 +1,211 @@
 import React from "react";
 
 export default function Page() {
-  return <div>Content</div>;
+  return (
+
+    <div className="grid grid-cols-10 bg-white relative">
+
+      <div className="col-span-8 sm:col-span-8 p-6">
+        <div className="flex items-center space-x-3 mb-4">
+          <h2>6 Contact Filter</h2>
+          <h1 className="font-bold text-2xl">AZ</h1>
+          <a href="" className="flex ml-auto border-b-2">Mostra tutti i contatti</a>
+        </div>
+
+        <div className="grid grid-cols-4 gap-x-4 my-3 items-center">
+          <div class="flex items-center space-x-3 justify-start">
+            <img src="/person1.jpeg" class="h-[50px] w-[50px] rounded-full" alt="Matteo Mirabella" />
+            <p class="font-semibold">Oktay Aydın</p>
+          </div>
+          <div>aaydinoktay@gmail.com</div>
+          <div>Funnel Consluting</div>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="#02FFD0" class="h-8 w-8">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+            </svg>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 gap-x-4 bg-gray-100 my-3 items-center">
+          <div class="flex items-center space-x-3 justify-start ">
+            <img src="/mattia.jpg" class="h-[50px] w-[50px] rounded-full " alt="Matteo Mirabella" />
+            <p class="font-semibold">Mattia Noris</p>
+          </div>
+          <div className="items-center">aaydinoktay@gmail.com</div>
+          <div className="items-center" >Funnel Consluting</div>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="#02FFD0" class="h-8 w-8">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+            </svg>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 gap-x-4 my-3 items-center">
+          <div class="flex items-center space-x-3 justify-start">
+            <img src="/person1.jpeg" class="h-[50px] w-[50px] rounded-full" alt="Matteo Mirabella" />
+            <p class="font-semibold">Oktay Aydın</p>
+          </div>
+          <div>aaydinoktay@gmail.com</div>
+          <div>Funnel Consluting</div>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="#02FFD0" class="h-8 w-8">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+            </svg>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 gap-x-4 my-3 bg-gray-100 items-center">
+          <div class="flex items-center space-x-3 justify-start">
+            <img src="/mattia.jpg" class="h-[50px] w-[50px] rounded-full" alt="Matteo Mirabella" />
+            <p class="font-semibold">Mattia Noris</p>
+          </div>
+          <div>aaydinoktay@gmail.com</div>
+          <div>Funnel Consluting</div>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="#02FFD0" class="h-8 w-8">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+            </svg>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 gap-x-4 my-3 items-center">
+          <div class="flex items-center space-x-3 justify-start">
+            <img src="/person1.jpeg" class="h-[50px] w-[50px] rounded-full" alt="Matteo Mirabella" />
+            <p class="font-semibold">Oktay Aydın</p>
+          </div>
+          <div>aaydinoktay@gmail.com</div>
+          <div>Funnel Consluting</div>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="#02FFD0" class="h-8 w-8">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+            </svg>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 gap-x-4 my-3 bg-gray-100 items-center">
+          <div class="flex items-center space-x-3 justify-start">
+            <img src="/mattia.jpg" class="h-[50px] w-[50px] rounded-full" alt="Matteo Mirabella" />
+            <p class="font-semibold">Mattia Noris</p>
+          </div>
+          <div>aaydinoktay@gmail.com</div>
+          <div>Funnel Consluting</div>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="#02FFD0" class="h-8 w-8">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+            </svg>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="col-span-2 sm:col-span-2 bg-gray-50 lg:flex lg:flex-col hidden">
+        <div className="flex flex-col col-span-1 px-5 border-b-2 py-4">
+          <div className="xl:flex xl:justify-between grid justify-center gap-2 w-full items-center ">
+            <div className="flex gap-2">
+              <div className="flex flex-col">
+                {" "}
+                <h1 className="text-lg font-bold">Account</h1>
+                <h3 className="text-xs">SalesPark</h3>{" "}
+              </div>
+              <img
+                alt=""
+                src={"/person1.jpeg"}
+                className="h-12 w-12 rounded-full  border-2 col-span-1 cursor-pointer"
+              />
+            </div>
+
+            <div className="avatar-group2  mx-2 lg:flex justify-end">
+              <div className="avatar2">
+                <img src="https://pixinvent.com/materialize-material-design-admin-template/app-assets/images/user/12.jpg" />
+              </div>
+
+              <div className="avatar2">
+                <img src="https://da4e1j5r7gw87.cloudfront.net/wp-content/uploads/sites/768/2018/08/glasses-american-man-20s-hipster.jpg" />
+              </div>
+              <div className="hidden-avatars2">+3</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col col-span-1 px-5 border-b-2 py-4">
+          <h1 className="font-bold text-xl">Delgatti Contatto</h1>
+          <a href="www.salespark.com">
+            <div className="flex gap-2 mt-2 cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="#02FFD0" class="h-8 w-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+              </svg>
+              <p>www.salespark.com</p>
+            </div>
+          </a>
+          <div className="flex justify-end gap-1 mt-5">
+            {" "}
+            <button className="bg-[#02FFD1] border-2 px-5 text-xs py-1">
+              Edit
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-col col-span-1 px-5 border-b-2 py-4">
+          <h1 className="font-bold text-xl">Pozisinone</h1>
+          <div className="flex fle-col">
+            <h3 class="font-bold text-sm">Inquadramento:</h3>
+            <p class="mt-1">Lorem ipsum dolor sit amet</p>
+          </div>
+          <div className="flex fle-col mt-2">
+            <h3 className="font-bold text-sm"> Azienda : </h3>
+            <p> Lorem ipsum dolor </p>
+          </div>
+          <div className="flex justify-end gap-1 mt-5">
+            {" "}
+            <button className="bg-[#02FFD1] border-2 px-5 text-xs py-1">
+              Edit
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-col col-span-1 px-5  py-4">
+          <h1 className="font-bold text-xl">Workflow</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, suscipit.</p>
+          <div className="flex justify-end gap-1 mt-5">
+            {" "}
+            <button className="bg-[#02FFD1] border-2 px-5 text-xs py-1">
+              Edit
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-col col-span-1 px-5 border-b-2 mt-4">
+          <div className="flex justify-between">
+            <h1 className="font-bold">Tags</h1>
+            <button className="bg-[#02FFD1] border-2 px-2 text-xs py-1 ">
+              +
+            </button>
+          </div>
+            <p className="text-sm">Lorem ipsum dolor sit</p>
+        </div>
+
+        <div className="flex flex-col col-span-1 px-5 border-b-2 mt-4">
+          <div className="flex justify-between">
+            <h1 className="font-bold">Contatti in Comune</h1>
+            <button className="bg-[#02FFD1] border-2 px-2 text-xs py-1 ">
+              +
+            </button>
+          </div>
+            <p className="text-sm">Lorem ipsum dolor sit</p>
+        </div>
+
+        <div className="flex flex-col col-span-1 px-5 border-b-2 mt-4">
+          <div className="flex justify-between">
+            <h1 className="font-bold">Team</h1>
+            <button className="bg-[#02FFD1] border-2 px-2 text-xs py-1 ">
+              +
+            </button>
+          </div>
+            <p className="text-sm">Lorem ipsum dolor sit</p>
+        </div>
+      </div>
+    </div>
+
+
+  )
 }
